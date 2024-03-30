44e5c961e19e1f10bf2fb60760e7823f8c030a71
TONYA DI SERA <tony.disera@gmail.com>
2024-03-29 20:24:54 -0600

    Remove S3 files before uploading

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ npm run build
 if [[ $1 == "prod" ]]; then
 
   echo "** Uploaded to prod s3 bucket **"
+  aws s3 rm s3://static.iobio.io/prod/rnasplice.iobio.io/assets/ --recursive
   aws s3 cp ./dist/  s3://static.iobio.io/prod/rnasplice.iobio.io/ --recursive
   echo "** Renew cloudfrount cache **"
   aws cloudfront create-invalidation --distribution-id E21P17I3OQG601 --paths /\*
@@ -23,13 +24,15 @@ if [[ $1 == "prod" ]]; then
 elif [[ $1 == "stage" ]]; then
 
   echo "** Uploaded to stage s3 bucket **"
+  aws s3 rm s3://static.iobio.io/stage/rnasplice.iobio.io/assets/ --recursive
   aws s3 cp ./dist/  s3://static.iobio.io/stage/rnasplice.iobio.io/ --recursive
   echo "** Renew cloudfrount cache **"
   aws cloudfront create-invalidation --distribution-id E1QTNXGVER3ZJT --paths /\*
 
 else
   echo "** Syncing to dev s3 bucket **"
-  aws s3 cp  ./dist/  s3://static.iobio.io/dev/splicejunction.iobio.io/ --recursive
+  aws s3 rm s3://static.iobio.io/dev/rnasplice.iobio.io/assets/ --recursive
+  aws s3 cp  ./dist/  s3://static.iobio.io/dev/rnasplice.iobio.io/ --recursive
   echo "** Renew cloudfrount cache **"
   aws cloudfront create-invalidation --distribution-id ???? --paths /\*
 fi
